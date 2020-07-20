a0105a986da614b56240f62a733dbbc69bc0b6ce
ub313stu <53057463+ub313stu@users.noreply.github.com>
2020-07-20 16:37:14 -0400

    Update doHistos.C

--- Root/doHistos.C
+++ Root/doHistos.C
@@ -32,8 +32,6 @@ void doHistos::Loop(std::string s_sample,bool isMC)
       float ht=0;
       float lead_jet_pt=0;
       float njets=0;
-      float jet_rads=0;
-      float sum_pt=0;
       TLorentzVector jet_p4;
       std::vector<Jet> jets; jets.clear();
       for (unsigned int i = 0; i <Jets_ID->size(); i++)
@@ -50,26 +48,18 @@ void doHistos::Loop(std::string s_sample,bool isMC)
       	jet_p4 = Jets->at(i);
       	jet.p4 = jet_p4;
 	
-	TLorentzVector jet_p4; 
-	jet_p4.SetPtEtaPhiE(0., 0., 0., 0.);
-	      
-	
-	      
-	sum_pt += jet_p4;
-	      
-	jet_rads+= jet_p4.DeltaR(constituent_p4)*jet_p4;
 
       	if (jet_p4.Pt() > lead_jet_pt ) lead_jet_pt = jet_p4.Pt() ;
 
       	jets.push_back(jet);
       }
 	
-      jet_width=jet_rads//sum_pt;
+ 
 	   
       plotter.Plot1D(Form("%s_testHT",s_sample.c_str()),";H_{T} [GeV]", ht, 20,0,2000 );
       plotter.Plot1D(Form("%s_HT"    ,s_sample.c_str()),";H_{T} [GeV]", HT, 20,0,2000 );
       plotter.Plot1D(Form("%s_njets" ,s_sample.c_str()),";n_{jets}", njets, 20,-0.5,19.5 );
-      plotter.Plot1D(Form("%s_jet_width",,s_sample.c_str()),";{jets}_rad", jet_width, 20, 0, 2000);
+
 
       /*
       Fat jets
